9b64d65ac103f5dff029a3148bc70ffab2099e6c
CTXDEV\Pierre.Bougattaya <pierre-hakim_bougattaya@dialog.com.au>
2017-01-06 11:16:13 +1000

    initial commit

---
+++ presentation/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Optimization;
+
+namespace Presentation.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //JS Bundle
+            var jsBundles = new ScriptBundle("~/bundles/js").Include(
+                                                "~/Scripts/jquery-{version}.js",
+                                                "~/Scripts/jquery-ui-{version}.js",
+                                                "~/Scripts/modernizr-{version}.js",
+                                                "~/Scripts/bootstrap.min.js");
+            bundles.Add(jsBundles);
+
+            //CSS Bundle
+            var cssBundles = new StyleBundle("~/bundles/css").Include(
+                                                "~/Content/bootstrap.min.css",
+                                                "~/Content/font-awesome.min.css",
+                                                "~/Content/animate.css",
+                                                "~/Content/Site.css");
+            bundles.Add(cssBundles);
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
